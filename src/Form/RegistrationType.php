7576927d696324a531d41d0f60e1248a985376a9
Alexandre Leote <alexandreleoteweb@gmail.com>
2025-02-26 13:24:22 +0100

    ✨ Amélioration de la gestion des accès non autorisés
    - Implémentation d'un gestionnaire personnalisé pour les exceptions d'accès
    - Création d'un EventListener pour intercepter les tentatives d'accès non autorisées
    - Ajout d'un message flash personnalisé lors du refus d'accès
    - Redirection automatique vers la page d'accueil en cas de droits insuffisants
    - Amélioration de la sécurité et de l'expérience utilisateur
    
    Modifications techniques :
    - Remplacement de l'EventSubscriber par un EventListener configurable
    - Configuration du listener dans services.yaml
    - Gestion centralisée des exceptions d'accès
    - Suppression des implémentations précédentes redondantes

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -3,24 +3,93 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Doctrine\DBAL\Types\StringType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('firstname')
-            ->add('email')
-            ->add('phone')
-            ->add('streetNumber')
-            ->add('streetName')
-            ->add('city')
-            ->add('zip')
-            ->add('region')
+            ->add('name', StringType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un nom',
+                    ]),
+                ],
+            ])
+            ->add('firstname', StringType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un prénom',
+                    ]),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email',
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un email',
+                    ]),
+                ],
+            ])
+            ->add('phone', TelType::class, [
+                'label' => 'N° de téléphone',
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un numéro de téléphone',
+                    ]),
+                ],
+            ])
+            ->add('streetNumber', StringType::class, [
+                'label' => 'N° de rue',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('streetName', StringType::class, [
+                'label' => 'Nom de rue',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('city', StringType::class, [
+                'label' => 'Ville',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('zip', StringType::class, [
+                'label' => 'Code postal',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('region', StringType::class, [
+                'label' => 'Région',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
         ;
     }
 
